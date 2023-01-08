397edc6ac136db2dd82e94ceddafb2687fbbb246
RegistryROK <registry.forbusiness@gmail.com>
2023-01-09 03:04:03 +0900

    rename: sign-in, sign-up move to merchant

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { SignInModule } from './sign-in/sign-in.module';
 import { SignUpModule } from './sign-up/sign-up.module';
 import { DomainModule } from './domain/domain.module';
+import { MerchantModule } from './merchant/merchant.module';
 
 @Module({
-  imports: [SignInModule, SignUpModule, DomainModule],
+  imports: [SignInModule, SignUpModule, DomainModule, MerchantModule],
 })
 export class AuthModule {}
